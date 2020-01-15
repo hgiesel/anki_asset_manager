a53cfa15c90ed53fe9253d87b7bb51a562b3798a
Henrik Giesel <hengiesel@gmail.com>
2020-01-15 13:05:34 +0100

    Add first try at meta scripts

---
+++ src/lib/interface.py
@@ -0,0 +1,48 @@
+from .types import SMInterface, SMMetaScript, SMScriptBool
+
+_meta_interfaces = []
+_meta_scripts = []
+
+def register_interface(
+    # name for the type of the interface
+    tag: str,
+    # function (id, note type, e.g. 'Standard', card type, e.g. 'FromQuestion', 'qfmt' | 'afmt')
+    generator: 'Callable[[id, model, tmpl, fmt]] -> str or False]',
+    # getting a SMScript with id
+    getter: 'Callable[[id, SMScriptStore] -> SMScript]',
+    # setting a SMScript with id
+    setter: 'Callable[[id, SMScript] -> None]',
+    # if False then cannot be deleted, else, the function is called with id
+    deleteable: False or 'Callable[[id, SMScript] -> None]',
+    # list of values that are readonly
+    # can contain: 'enabled', 'name', 'version', 'description', 'conditions', 'code'
+    readonly: SMScriptBool,
+    # list of values that are stored in `store` field
+    # can contain: 'enabled', 'name', 'version', 'description', 'conditions', 'code'
+    store: SMScriptBool,
+):
+    _meta_interfaces.append(SMInterface(
+        tag,
+        generator,
+        getter,
+        setter,
+        deleteable,
+        readonly,
+        store,
+    ))
+
+def get_interface(tag):
+    try:
+        return next(filter(lambda v: v.tag == tag, _meta_interfaces))
+    except:
+        return None
+
+def add_meta_script(tag, id):
+    if get_interface(tag) != None:
+        _meta_scripts.append(SMMetaScript(tag, id))
+
+def get_meta_scripts():
+    return _meta_scripts
+
+from aqt.utils import showInfo
+showInfo(_meta_interfaces)
--- src/lib/types.py
+++ src/lib/types.py
@@ -4,13 +4,50 @@ import attr
 class SMSetting:
     model_name: str = attr.ib()
     scripts: list = attr.ib()
-    meta: dict = attr.ib(default = {})
 
 @attr.s(slots=True, frozen=True)
 class SMScript:
-    enabled: bool = attr.ib()
-    name: str = attr.ib()
-    version: str = attr.ib()
-    description: str = attr.ib()
-    conditions: list = attr.ib()
-    code: str = attr.ib()
+    enabled: bool = attr.ib(False)
+    name: str = attr.ib('')
+    version: str = attr.ib('')
+    description: str = attr.ib('')
+    conditions: list = attr.ib([])
+    code: str = attr.ib('')
+
+################################
+
+@attr.s(slots=True, frozen=True)
+class SMInterface:
+    tag: str = attr.ib()
+    generator = attr.ib()
+    getter = attr.ib()
+    setter = attr.ib()
+    deleteable: bool or 'function' = attr.ib()
+    readonly: SMScriptBool = attr.ib(default = SMScriptBool())
+    store: SMScriptBool = attr.ib(default = SMScriptBool())
+
+################################
+
+@attr.s(slots=True, frozen=True)
+class SMMetaScript:
+    tag: str = attr.ib()
+    id: str = attr.ib()
+    storage: SMScriptStore = attr.ib(default=SMScriptStore())
+
+@attr.s(slots=True, frozen=True)
+class SMScriptBool:
+    enabled: bool = attr.ib(False)
+    name: bool = attr.ib(False)
+    version: bool = attr.ib(False)
+    description: bool = attr.ib(False)
+    conditions: bool = attr.ib(False)
+    code: bool = attr.ib(False)
+
+@attr.s(slots=True, frozen=True)
+class SMScriptStore:
+    enabled: bool = attr.ib(default=None)
+    name: str = attr.ib(default=None)
+    version: str = attr.ib(default=None)
+    description: str = attr.ib(default=None)
+    conditions: list = attr.ib(default=None)
+    code: str = attr.ib(default=None)
