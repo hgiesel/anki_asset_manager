5c2959a61004358d16601a68526be59afc4d78ab
Henrik Giesel <hengiesel@gmail.com>
2020-09-01 13:09:10 +0200

    Give condition parser its own file

---
+++ src/lib/condition_parser.py
@@ -0,0 +1,183 @@
+from functools import reduce
+
+
+def get_condition_parser(card, position):
+    is_true = lambda v: isinstance(v, bool) and v == True
+    is_false = lambda v: isinstance(v, bool) and v == False
+
+    # [needscondection, newcondection]
+    def parse_condition(cond) -> [bool, list]:
+        if len(cond) == 0:
+            # empty conditions
+            return True, cond
+
+        elif isinstance(cond, bool):
+            return cond, cond
+
+        elif cond[0] == '&':
+            parsed_conds = [parse_condition(i) for i in cond[1:]]
+            truth_result = reduce(lambda x, y: x and y, [res[0] for res in parsed_conds])
+            parsed_result = [res[1] for res in parsed_conds]
+
+            if any([is_false(pr) for pr in parsed_result]):
+                # and condition contains False
+                result = False
+            else:
+                # filter out False
+                result = list(filter(lambda v: not is_true(v), parsed_result))
+                if len(result) > 1:
+                    # reinsert "&"
+                    result.insert(0, cond[0])
+                else:
+                    # flatten list, drop "&"
+                    result = [item for sublist in result for item in sublist]
+
+            return truth_result, result
+
+        elif cond[0] == '|':
+            parsed_conds = [parse_condition(i) for i in cond[1:]]
+            truth_result =  reduce(lambda x, y: x or y, [res[0] for res in parsed_conds])
+            parsed_result = [res[1] for res in parsed_conds]
+
+            if any([is_true(pr) for pr in parsed_result]):
+                # or condition contains True
+                result = True
+            else:
+                # filter out True
+                result = list(filter(lambda v: not is_false(v), parsed_result))
+                if len(result) > 1:
+                    result.insert(0, cond[0])
+                else:
+                    result = [item for sublist in result for item in sublist]
+
+            return truth_result, result
+
+        elif cond[0] == '!':
+            parsed_cond = parse_condition(cond[1])
+
+            if isinstance(parsed_cond[1], bool):
+                return True, not parsed_cond[1]
+            else:
+                return True, [cond[0], parsed_cond[1]]
+
+        elif cond[0] == 'card':
+            if cond[1] == '=':
+                val = card == cond[2]
+
+            elif cond[1] == '!=':
+                val = card != cond[2]
+
+            elif cond[1] == 'includes':
+                val = cond[2] in card
+
+            elif cond[1] == 'startsWith':
+                val = card.startswith(cond[2])
+
+            elif cond[1] == 'endsWith':
+                val = card.endswith(cond[2])
+
+            return val, val
+
+        elif cond[0] == 'pos':
+            if cond[1] == '=':
+                if (position == cond[2]):
+                    return True, True
+
+                else:
+                    return False, False
+
+            elif cond[1] == '!=':
+                if (position != cond[2]):
+                    return True, True
+
+                else:
+                    return False, False
+
+        elif cond[0] == 'tag' or cond[0] == 'tagFull' or cond[0] == 'iter':
+            return True, cond
+
+    return parse_condition
+
+def stringify_conds(conds) -> str:
+    if len(conds) == 0:
+        return 'true'
+
+    elif isinstance(conds, bool) and conds:
+        return 'true'
+
+    elif isinstance(conds, bool) and not conds:
+        return 'false'
+
+    elif conds[0] == '&':
+        stringified_conds = [stringify_conds(c) for c in conds[1:]]
+        parsed_result = [res[1] for res in stringified_conds]
+        if 'false' in parsed_result:
+            return 'false'
+        else:
+            return ' && '.join([p for p in parsed_result if p != 'true'])
+
+    elif conds[0] == '|':
+        stringified_conds = [stringify_conds(c) for c in conds[1:]]
+        parsed_result = [res[1] for res in stringified_conds]
+        if 'true' in parsed_result:
+            return 'true'
+        else:
+            return ' || '.join([p for p in parsed_result if p != 'false'])
+
+    elif conds[0] == '!':
+        stringed_cond = stringify_conds(conds[1])
+
+        if stringed_cond == 'false':
+            return 'true'
+        elif stringed_cond == 'true':
+            return 'false'
+        else:
+            return f'!({stringed_cond})'
+
+    elif conds[0] == 'card':
+        if conds[1] == '=':
+            return f"'{{{{Card}}}}' === {conds[2]}"
+
+        elif conds[1] == '!=':
+            return f"'{{{{Card}}}}' === {conds[2]}"
+
+        elif conds[1] == 'includes':
+            return f"'{{{{Card}}}}'.includes('{conds[2]}')"
+
+        elif conds[1] == 'startsWith':
+            return f"'{{{{Card}}}}'.startsWith('{conds[2]}')"
+
+        elif conds[1] == 'endsWith':
+            return f"'{{{{Card}}}}'.endsWith('{conds[2]}')"
+
+    elif conds[0] == 'tagString':
+        if conds[1] == '=':
+            return f"'{{{{Tags}}}}' === {conds[2]}"
+
+        elif conds[1] == '!=':
+            return f"'{{{{Tags}}}}' === {conds[2]}"
+
+        elif conds[1] == 'includes':
+            return f"'{{{{Tags}}}}'.includes('{conds[2]}')"
+
+        elif conds[1] == 'startsWith':
+            return f"'{{{{Tags}}}}'.startsWith('{conds[2]}')"
+
+        elif conds[1] == 'endsWith':
+            return f"'{{{{Tags}}}}'.endsWith('{conds[2]}')"
+
+    elif conds[0] == 'tag':
+        if conds[1] == '=':
+            return f"'{{{{Tags}}}}'.split(' ').includes('{conds[2]}')"
+
+        elif conds[1] == '!=':
+            return f"'!{{{{Tags}}}}'.split(' ').includes('{conds[2]}')"
+
+        elif conds[1] == 'includes':
+            return f"'{{{{Tags}}}}'.split(' ').some(v => v.includes('{conds[2]}'))"
+
+        elif conds[1] == 'startsWith':
+            return f"'{{{{Tags}}}}'.split(' ').some(v => v.startsWith('{conds[2]}'))"
+
+        elif conds[1] == 'endsWith':
+            return f"'{{{{Tags}}}}'.split(' ').some(v => v.endsWith('{conds[2]}'))"
--- src/lib/stringify.py
+++ src/lib/stringify.py
@@ -1,202 +1,18 @@
 from typing import Optional, Union, Literal
-from functools import reduce
-
-from .registrar import get_interface
 
 from ..config_types import Setting, ConcreteScript, ScriptInsertion, ScriptPosition, Fmt
 from ..utils import version
 
-def get_condition_parser(card, position):
-    is_true = lambda v: isinstance(v, bool) and v == True
-    is_false = lambda v: isinstance(v, bool) and v == False
-
-    # [needscondection, newcondection]
-    def parse_condition(cond) -> [bool, list]:
-        if len(cond) == 0:
-            # empty conditions
-            return True, cond
-
-        elif isinstance(cond, bool):
-            return cond, cond
-
-        elif cond[0] == '&':
-            parsed_conds = [parse_condition(i) for i in cond[1:]]
-            truth_result = reduce(lambda x, y: x and y, [res[0] for res in parsed_conds])
-            parsed_result = [res[1] for res in parsed_conds]
-
-            if any([is_false(pr) for pr in parsed_result]):
-                # and condition contains False
-                result = False
-            else:
-                # filter out False
-                result = list(filter(lambda v: not is_true(v), parsed_result))
-                if len(result) > 1:
-                    # reinsert "&"
-                    result.insert(0, cond[0])
-                else:
-                    # flatten list, drop "&"
-                    result = [item for sublist in result for item in sublist]
-
-            return truth_result, result
-
-        elif cond[0] == '|':
-            parsed_conds = [parse_condition(i) for i in cond[1:]]
-            truth_result =  reduce(lambda x, y: x or y, [res[0] for res in parsed_conds])
-            parsed_result = [res[1] for res in parsed_conds]
-
-            if any([is_true(pr) for pr in parsed_result]):
-                # or condition contains True
-                result = True
-            else:
-                # filter out True
-                result = list(filter(lambda v: not is_false(v), parsed_result))
-                if len(result) > 1:
-                    result.insert(0, cond[0])
-                else:
-                    result = [item for sublist in result for item in sublist]
-
-            return truth_result, result
-
-        elif cond[0] == '!':
-            parsed_cond = parse_condition(cond[1])
-
-            if isinstance(parsed_cond[1], bool):
-                return True, not parsed_cond[1]
-            else:
-                return True, [cond[0], parsed_cond[1]]
-
-        elif cond[0] == 'card':
-            if cond[1] == '=':
-                val = card == cond[2]
-
-            elif cond[1] == '!=':
-                val = card != cond[2]
-
-            elif cond[1] == 'includes':
-                val = cond[2] in card
-
-            elif cond[1] == 'startsWith':
-                val = card.startswith(cond[2])
-
-            elif cond[1] == 'endsWith':
-                val = card.endswith(cond[2])
-
-            return val, val
-
-        elif cond[0] == 'pos':
-            if cond[1] == '=':
-                if (position == cond[2]):
-                    return True, True
-
-                else:
-                    return False, False
-
-            elif cond[1] == '!=':
-                if (position != cond[2]):
-                    return True, True
-
-                else:
-                    return False, False
-
-        elif cond[0] == 'tag' or cond[0] == 'tagFull' or cond[0] == 'iter':
-            return True, cond
-
-    return parse_condition
-
-def stringify_conds(conds) -> str:
-    if len(conds) == 0:
-        return 'true'
-
-    elif isinstance(conds, bool) and conds:
-        return 'true'
-
-    elif isinstance(conds, bool) and not conds:
-        return 'false'
-
-    elif conds[0] == '&':
-        stringified_conds = [stringify_conds(c) for c in conds[1:]]
-        parsed_result = [res[1] for res in stringified_conds]
-        if 'false' in parsed_result:
-            return 'false'
-        else:
-            return ' && '.join([p for p in parsed_result if p != 'true'])
-
-    elif conds[0] == '|':
-        stringified_conds = [stringify_conds(c) for c in conds[1:]]
-        parsed_result = [res[1] for res in stringified_conds]
-        if 'true' in parsed_result:
-            return 'true'
-        else:
-            return ' || '.join([p for p in parsed_result if p != 'false'])
-
-    elif conds[0] == '!':
-        stringed_cond = stringify_conds(conds[1])
-
-        if stringed_cond == 'false':
-            return 'true'
-        elif stringed_cond == 'true':
-            return 'false'
-        else:
-            return f'!({stringed_cond})'
-
-    elif conds[0] == 'card':
-        if conds[1] == '=':
-            return f"'{{{{Card}}}}' === {conds[2]}"
-
-        elif conds[1] == '!=':
-            return f"'{{{{Card}}}}' === {conds[2]}"
-
-        elif conds[1] == 'includes':
-            return f"'{{{{Card}}}}'.includes('{conds[2]}')"
-
-        elif conds[1] == 'startsWith':
-            return f"'{{{{Card}}}}'.startsWith('{conds[2]}')"
-
-        elif conds[1] == 'endsWith':
-            return f"'{{{{Card}}}}'.endsWith('{conds[2]}')"
-
-    elif conds[0] == 'tagString':
-        if conds[1] == '=':
-            return f"'{{{{Tags}}}}' === {conds[2]}"
-
-        elif conds[1] == '!=':
-            return f"'{{{{Tags}}}}' === {conds[2]}"
-
-        elif conds[1] == 'includes':
-            return f"'{{{{Tags}}}}'.includes('{conds[2]}')"
-
-        elif conds[1] == 'startsWith':
-            return f"'{{{{Tags}}}}'.startsWith('{conds[2]}')"
-
-        elif conds[1] == 'endsWith':
-            return f"'{{{{Tags}}}}'.endsWith('{conds[2]}')"
-
-    elif conds[0] == 'tag':
-        if conds[1] == '=':
-            return f"'{{{{Tags}}}}'.split(' ').includes('{conds[2]}')"
-
-        elif conds[1] == '!=':
-            return f"'!{{{{Tags}}}}'.split(' ').includes('{conds[2]}')"
-
-        elif conds[1] == 'includes':
-            return f"'{{{{Tags}}}}'.split(' ').some(v => v.includes('{conds[2]}'))"
-
-        elif conds[1] == 'startsWith':
-            return f"'{{{{Tags}}}}'.split(' ').some(v => v.startsWith('{conds[2]}'))"
-
-        elif conds[1] == 'endsWith':
-            return f"'{{{{Tags}}}}'.split(' ').some(v => v.endsWith('{conds[2]}'))"
+from .registrar import get_interface
+from .condition_parser import get_condition_parser, stringify_conds
 
 def wrap_code(code, conditions: Union[bool, list]):
     if ((type(conditions) is bool) and conditions) or len(conditions) == 0:
         return code
 
     else:
-        return (
-            f'if ({stringify_conds(conditions)}) {{\n' +
-            '\n'.join([f'    {line}' for line in code.split('\n')]) +
-            '\n}'
-        )
+        main_code = '\n'.join([f'    {line}' for line in code.split('\n')])
+        return f'if ({stringify_conds(conditions)}) {{\n{main_code}\n}}'
 
 def indent(line, indentation):
     return indentation + line if len(line) > 0 else line
@@ -209,10 +25,16 @@ def indent_lines(text: str, indent_size: int) -> str:
     ])
 
 def stringify_script_data(sd, indent_size: int, in_html: bool) -> str:
+    opening = f'<script {sd["tag"]}>\n' if in_html else f'// {sd["tag"]}\n'
+    closing = '</script>' if in_html else ''
+
+    wrapped_code = wrap_code(sd["code"], sd["conditions"])
+    indented_code = indent_lines(wrapped_code, indent_size if in_html else 0) + '\n'
+
     return (
-        (f'<script {sd["tag"]}>\n' if in_html else f'// {sd["tag"]}\n') +
-        f'{indent_lines(wrap_code(sd["code"], sd["conditions"]), indent_size if in_html else 0)}\n' +
-        ('</script>' if in_html else '')
+        opening +
+        indented_code +
+        closing
     )
 
 def encapsulate_scripts(scripts, version, indent_size) -> str:
@@ -245,45 +67,49 @@ def stringify_setting(
     cardtype_name: str,
     position: ScriptInsertion,
 ) -> str:
-    script_data = []
     the_parser = get_condition_parser(cardtype_name, position)
+    script_data = []
 
-    if setting.enabled and not setting.insert_stub:
-        for script in setting.scripts:
-            script_gotten = (
-                script
-                if isinstance(script, ConcreteScript)
-                else get_interface(script.tag).getter(
-                    script.id,
-                    script.storage,
-                )
+    if not setting.enabled or setting.insert_stub:
+        return script_data
+
+    for script in setting.scripts:
+        script_gotten = (
+            script
+            if isinstance(script, ConcreteScript)
+            else get_interface(script.tag).getter(
+                script.id,
+                script.storage,
             )
+        )
 
-            if script_gotten.enabled and script_gotten.position == ('into_template' if position in ['question', 'answer'] else position):
-                needs_inject, conditions_simplified = the_parser(script_gotten.conditions)
+        if script_gotten.enabled and script_gotten.position == ('into_template' if position in ['question', 'answer'] else position):
+            from aqt.utils import showText
+            showText(str(script_gotten.conditions) + '\n------------\n' + str(the_parser(script_gotten.conditions)))
+            needs_inject, conditions_simplified = the_parser(script_gotten.conditions)
 
-                if needs_inject:
-                    sd = {
-                        'tag': gen_data_attributes(
-                            script_gotten.name,
-                            script_gotten.version,
-                        ),
-                        'code': (
-                            script_gotten.code
-                            if isinstance(script, ConcreteScript)
-                            else get_interface(script.tag).generator(
-                                script.id,
-                                script.storage,
-                                model_name,
-                                cardtype_name,
-                                position,
-                            )
-                        ),
-                        'conditions': conditions_simplified,
-                    }
+            if needs_inject:
+                sd = {
+                    'tag': gen_data_attributes(
+                        script_gotten.name,
+                        script_gotten.version,
+                    ),
+                    'code': (
+                        script_gotten.code
+                        if isinstance(script, ConcreteScript)
+                        else get_interface(script.tag).generator(
+                            script.id,
+                            script.storage,
+                            model_name,
+                            cardtype_name,
+                            position,
+                        )
+                    ),
+                    'conditions': conditions_simplified,
+                }
 
-                    if len(sd['code']) > 0:
-                        script_data.append(sd)
+                if len(sd['code']) > 0:
+                    script_data.append(sd)
 
     stringified_scripts = [
         stringify_script_data(sd, setting.indent_size, True)
