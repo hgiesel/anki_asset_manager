3d30f17c6925a391cc070c5be82790812e51f85c
Henrik Giesel <hengiesel@gmail.com>
2020-09-02 23:31:15 +0200

    The prevent_reinclusion script must be on the front of the card
    * if it's on the back, then, by the time it's executed, it's too late

--- src/lib/stringify.py
+++ src/lib/stringify.py
@@ -139,7 +139,7 @@ def stringify_setting_for_template(
 ) -> str:
     stringified_scripts = stringify_setting(setting, model_name, cardtype_name, fmt)
 
-    if not setting.insert_stub and not fmt == 'question':
+    if not setting.insert_stub and fmt == 'question':
         stringified_scripts.insert(0, stringify_script_data(prevent_reinclusion, setting.indent_size, True))
 
     code_string = encapsulate_scripts(
