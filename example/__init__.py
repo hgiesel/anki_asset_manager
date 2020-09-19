e61e778096672c8c3a496b1551b73586fdecf6e7
Henrik Giesel <hengiesel@gmail.com>
2020-09-19 02:09:08 +0200

    Fix example (adapt to Asset Manager rather than Script manager)

--- example/__init__.py
+++ example/__init__.py
@@ -1,5 +1,5 @@
 from aqt import mw
-from anki.hooks import addHook
+from aqt.gui_hooks import profile_did_open
 
 from .utils import find_addon_by_name
 
@@ -13,97 +13,104 @@ if am:
     ami = __import__(am).src.lib.interface
     amr = __import__(am).src.lib.registrar
 
+def get_script() -> str:
+    from pathlib import Path
+    from os.path import dirname, realpath
+
+    filepath = Path(dirname(realpath(__file__)), f'{script_name}.js')
+
+    with open(filepath, 'r') as file:
+        return file.read().strip()
+
 def setup_script():
     if not am:
         return
 
-    from pathlib import Path
-    from os.path import dirname, realpath
+    script = get_script()
 
-    filepath = Path(f'{dirname(realpath(__file__))}', f'{script_name}.js')
+    my_interface = ami.make_interface(
+        # The name of script tag
+        # Multiple scripts can be registered under the same tag
+        # Scripts under one tag share one *interface*: rules for setting, getting, generation, stored fields, readonly fields, etc.
+        tag = f"{script_name}_tag",
 
-    with open(filepath, 'r') as file:
-        script = file.read().strip()
+        # What happens when the user tries to receive the script
+        # This is is used for displaying the script in the tag window
+        # the code is not necessarily the code that is actually inserted into the template: for that, see `generator`
+        # however the conditions are used for calculating whether to insert
+        getter = lambda id, storage: ami.make_script(
+            script_name,
+            storage.enabled if storage.enabled is not None else True,
+            'js',
+            version,
+            description,
+            storage.position if storage.position is not None else 'into_template',
+            storage.conditions if storage.conditions is not None else [],
+            storage.code if storage.code is not None else script,
+        ),
 
-        amr.register_interface(
-            ami.make_interface(
-                # The name of script tag
-                # Multiple scripts can be registered under the same tag
-                # Scripts under one tag share one *interface*: rules for setting, getting, generation, stored fields, readonly fields, etc.
-                tag = f"{script_name}_tag",
+        # What happens when the user commits new changes to the script
+        # Can be used for internal computation
+        # returns a bool or ami.Script.
+        # if returns True all fields defined in `store` are stored
+        # if returns False no fields are stored ever
+        # if returns ami.Script, this Script is used for saving, otherwise it's the same as the argument
+        setter = lambda id, script: True,
 
-                # What happens when the user tries to receive the script
-                # This is is used for displaying the script in the tag window
-                # the code is not necessarily the code that is actually inserted into the template: for that, see `generator`
-                # however the conditions are used for calculating whether to insert
-                getter = lambda id, storage: ami.make_script(
-                    storage.enabled if storage.enabled is not None else True,
-                    script_name,
-                    version,
-                    description,
-                    storage.conditions if storage.conditions is not None else [],
-                    storage.code if storage.code is not None else script,
-                ),
+        # Collection of fields that are stored by Script Manager
+        store = ['enabled', 'code', 'position', 'conditions'],
+        # Collection of fields that are read-only
+        readonly = ['name', 'type', 'version', 'description'],
 
-                # What happens when the user commits new changes to the script
-                # Can be used for internal computation
-                # returns a bool or ami.Script.
-                # if returns True all fields defined in `store` are stored
-                # if returns False no fields are stored ever
-                # if returns ami.Script, this Script is used for saving, otherwise it's the same as the argument
-                setter = lambda id, script: True,
+        # Change the code that is showed in the script window
+        # By default it is "your_tag: your_id"
+        # label = lambda id, storage: your code that returns str
 
-                # Collection of fields that are stored on the side of Script Manager
-                store = ['enabled', 'code', 'conditions'],
-                # Collection of fields that are readonly
-                readonly = ['name', 'version', 'description'],
+        # Change the behavior when deleting the script
+        # By default your script is not deletable
+        # deletable = lambda id, storage: your code that returns bool (whether to delete or not)
 
-                # Change the code that is showed in the script window
-                # By default is "your_tag: your_id"
-                # label = lambda id, storage: your code that returns str
+        # Change the behavior when resetting the script
+        # By default your script is reset to the getter function with an empty storage
+        # this reset function does not reset the enabled status or the conditions
+        reset = lambda id, storage: ami.make_script(
+            script_name,
+            storage.enabled if storage.enabled else True,
+            'js',
+            version,
+            description,
+            'into_template',
+            storage.conditions if storage.conditions is not None else [],
+            script,
+        ),
+        # ...or...
+        # reset = False (your code cannot be reset + reset button is hidden)
 
-                # Change the behavior when deleting the script
-                # By default your script is not deletable
-                # deletable = lambda id, storage: your code that returns bool (whether to delete or not)
+        # Change the behavior when generating the script that is ultimately inserted into the template
+        # By default it uses `getter(id, storage).code`
+        # model is the note type name, tmpl is the card type name, fmt is 'qfmt' (front) or 'afmt' (back)
+        # if your return an empty str, it won't insert anything
+        # generator = lambda id, storage, model, tmpl, fmt: your code that returns a str (that is then inserted into the template)
+    )
 
-                # Change the behavior when resetting the script
-                # By default your script is reset to the getter function with an empty storage
-                # this reset function does not reset the enabled status or the conditions
-                reset = lambda id, storage: ami.make_script(
-                    storage.enabled if storage.enabled else True,
-                    script_name,
-                    version,
-                    description,
-                    storage.conditions if storage.conditions is not None else [],
-                    script,
-                ),
-                # ...or...
-                # reset = False (your code cannot be reset + reset button is hidden)
-
-                # Change the behavior when generating the script that is ultimately inserted into the template
-                # By default it uses `getter(id, storage).code`
-                # model is the note type name, tmpl is the card type name, fmt is 'qfmt' (front) or 'afmt' (back)
-                # if your return an empty str, it won't insert anything
-                # generator = lambda id, storage, model, tmpl, fmt: your code that returns a str (that is then inserted into the template)
-            )
-        )
+    amr.register_interface(my_interface)
 
 def install_script():
     # create the meta script which points to your interface
     my_meta_script = ami.make_meta_script(
-        # this is the tag you interface above is registered on!
+        # this is the tag your interface above is registered on!
         f"{script_name}_tag",
         # your id: you can register an id only once per model per tag
         f"{script_name}_id",
     )
 
     # insert the script for every model
-    for model_name in mw.col.models.allNames():
+    for model_id in mw.col.models.ids():
         amr.register_meta_script(
-            model_name,
+            model_id,
             my_meta_script,
         )
 
 if am:
     setup_script()
-    addHook('profileLoaded', install_script)
+    profile_did_open.append(install_script)
