ddc36f5e5a60f963a607809a8863212e81f4214d
Henrik Giesel <hengiesel@gmail.com>
2020-12-07 14:33:19 +0100

    Split opening functionality into extra function

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+from aqt.qt import QDialog
 from aqt.models import Models
 from aqt.gui_hooks import models_did_init_buttons
 
@@ -9,20 +10,23 @@ from .config import (
 )
 
 
-def on_assets(models: Models) -> None:
-    current_row = models.form.modelsList.currentRow()
-    current_notetype = models.mm.get(models.models[current_row].id)
-
-    dialog = ConfigDialog(models.mw)
+def open_asset_manager_menu(parent_window: QDialog, note_type):
+    dialog = ConfigDialog(parent_window)
 
     dialog.setupUi(
-        current_notetype["id"],
-        current_notetype["name"],
-        get_html_setting_from_notetype(current_notetype),
-        get_setting_from_notetype(current_notetype),
+        note_type["id"],
+        note_type["name"],
+        get_html_setting_from_notetype(note_type),
+        get_setting_from_notetype(note_type),
     )
 
     dialog.exec_()
+
+
+def on_assets(models: Models) -> None:
+    current_row = models.form.modelsList.currentRow()
+    current_notetype = models.mm.get(models.models[current_row].id)
+    open_asset_manager_menu(models.mw, current_notetype)
 
 
 def init_asset_button(buttons, models: Models):
