f56d3ddaaedc7100e3eeec7063be443c97117ca9
Henrik Giesel <hengiesel@gmail.com>
2021-03-11 16:58:44 +0100

    Fix minification

---
+++ src/model_editor/minifier.py
@@ -0,0 +1,110 @@
+from typing import List
+import json
+
+from aqt import mw
+from aqt.webview import AnkiWebView
+from aqt.utils import showInfo, showWarning, tooltip
+
+from .common import write_model_template
+
+# # DEBUG
+# mw.mainLayout.addWidget(minifier, 1)
+# mw.minifier = minifier
+
+def minify_command(unminifieds: List[str]) -> str:
+    escaped = (
+        "["
+        + (
+            ",".join(
+                [
+                    '"' + unminified.replace('"', '\\"').replace("\n", "\\n") + '"'
+                    for unminified in unminifieds
+                ]
+            )
+        )
+        + "]"
+    )
+
+    result = f"pycmd(`insertMinifieds::${{JSON.stringify({escaped}.map(html => minify(html, minifyOptions)))}}`)"
+    return result
+
+
+def process_minifieds(minifieds: List[str], template_fmts, callback):
+    for index, minified in enumerate(minifieds):
+        template, fmt = template_fmts[index]
+        write_model_template(template, fmt, minified)
+
+    callback()
+
+
+def notify_minification(callback):
+    def inner():
+        tooltip("Finished insertion of minified HTML")
+        callback()
+
+    return inner
+
+
+class Minifier(AnkiWebView):
+    def __init__(
+        self,
+        parent,
+        template_fmts,
+        callback,
+    ):
+        AnkiWebView.__init__(self, parent=parent, title="minifier")
+        self.set_bridge_command(self.bridge_cmd, parent)
+
+        self.template_fmts = template_fmts
+        self.callback = callback
+
+    def bridge_cmd(self, cmd: str) -> None:
+        if cmd.startswith("insertMinifieds"):
+            _, code = cmd.split("::", 1)
+            data = json.loads(code)
+            process_minifieds(data, self.template_fmts, notify_minification(self.callback))
+
+    def minify(
+        self,
+        unminifieds,
+    ) -> None:
+        self.eval("""
+var minify = require("html-minifier-terser").minify
+var minifyOptions = {
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    continueOnParseError: true,
+    decodeEntities: true,
+    minifyCSS: true,
+    minifyJS: true,
+    processConditionalComments: true,
+    removeAttributeQuotes: true,
+    removeComments: true,
+    removeEmptyAttributes: true,
+    removeOptionalTags: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    removeTagWhitespace: true,
+    sortAttributes: true,
+    sortClassName: true,
+    trimCustomFragments: true,
+    useShortDoctype: true,
+}""")
+
+        cmd = minify_command(unminifieds)
+        self.eval(cmd)
+
+
+def get_minifier(parent, template_fmts, callback) -> Minifier:
+    minifier = Minifier(parent, template_fmts, callback)
+
+    addon_package = mw.addonManager.addonFromModule(__name__)
+    minifier.stdHtml(
+        "",
+        js=[
+            f"/_addons/{addon_package}/web/htmlminifier.js",
+        ],
+    )
+
+    return minifier
--- src/model_editor/minify.py
+++ src/model_editor/minify.py
@@ -1,87 +1,7 @@
-from typing import List
-import json
-
-from aqt import mw
-from aqt.webview import AnkiWebView
 from aqt.utils import tooltip
+from aqt import mw
 
-from anki.models import NoteType
-
-from .common import write_model_template
-
-from ..config_types import AnkiFmt
-
-import os
-
-
-def minify_command(unminifieds: List[str]) -> str:
-    escaped = (
-        "["
-        + (
-            ",".join(
-                [
-                    '"' + unminified.replace('"', '\\"').replace("\n", "\\n") + '"'
-                    for unminified in unminifieds
-                ]
-            )
-        )
-        + "]"
-    )
-
-    return f"JSON.stringify({escaped}.map(html => minify(html, minifyOptions)))"
-
-
-minifier = AnkiWebView(title="minify")
-addon_package = mw.addonManager.addonFromModule(__name__)
-
-minifier.stdHtml(
-    "",
-    js=[
-        f"/_addons/{addon_package}/web/htmlminifier.js",
-    ],
-)
-
-minifier.eval(
-    """
-var minify = require("html-minifier-terser").minify
-var minifyOptions = {
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    continueOnParseError: true,
-    decodeEntities: true,
-    minifyCSS: true,
-    minifyJS: true,
-    processConditionalComments: true,
-    removeAttributeQuotes: true,
-    removeComments: true,
-    removeEmptyAttributes: true,
-    removeOptionalTags: true,
-    removeRedundantAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    removeTagWhitespace: true,
-    sortAttributes: true,
-    sortClassName: true,
-    trimCustomFragments: true,
-    useShortDoctype: true,
-}"""
-)
-
-
-def process_minifieds(minifieds: List[str], template_fmts, callback):
-    for index, minified in enumerate(minifieds):
-        template, fmt = template_fmts[index]
-        write_model_template(template, fmt, minified)
-
-    callback()
-
-
-def notify_minification(callback):
-    def inner():
-        tooltip("Finished insertion of minified HTML")
-        callback()
-
-    return inner
+from .minifier import get_minifier, process_minifieds
 
 
 def insert_minified(
@@ -90,13 +10,8 @@ def insert_minified(
     callback,
 ) -> None:
     tooltip("Started insertion of minified HTML")
-    cmd = minify_command(unminifieds)
-    minifier.evalWithCallback(
-        cmd,
-        lambda minifieds: process_minifieds(
-            json.loads(minifieds), template_fmts, notify_minification(callback)
-        ),
-    )
+    minifier = get_minifier(mw, template_fmts, callback)
+    minifier.minify(unminifieds)
 
 
 def insert_unminified(
